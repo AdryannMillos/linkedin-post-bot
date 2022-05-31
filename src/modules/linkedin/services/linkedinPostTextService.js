c3d467bcbd8c1962b8a5109ba84c60f637dfef83
Adryann Millos <adryann312@gmail.com>
2022-05-31 09:41:47 -0300

    Add current hour log

--- src/modules/linkedin/services/linkedinPostTextService.js
+++ src/modules/linkedin/services/linkedinPostTextService.js
@@ -24,9 +24,9 @@ async function linkedinPost(text) {
   await page.click('.btn__primary--large');
   
 
-  await page.waitForSelector('button.btn__primary--large');
+  await page.waitForSelector('button.artdeco-button');
 
-  await page.click('button.btn__primary--large');
+  await page.click('button.artdeco-button');
 
 
   await page.waitForSelector('div.ql-editor.ql-blank');
