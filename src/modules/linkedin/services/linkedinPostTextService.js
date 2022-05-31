050fd1184efa10dd5c6c466278de744da257b787
Adryann Millos <adryann312@gmail.com>
2022-05-31 10:11:53 -0300

    Add current hour log

--- src/modules/linkedin/services/linkedinPostTextService.js
+++ src/modules/linkedin/services/linkedinPostTextService.js
@@ -4,12 +4,10 @@ const  { setTimeout } = require('timers/promises');
 require("dotenv").config();
 
 async function linkedinPost(text) {
-  const browser = await puppeteer.launch({
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-    ],
-  });  
+  const browser = await puppeteer.launch({args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+  ]},);
   const page = await browser.newPage();
 
   await page.goto('https://www.linkedin.com/uas/login?fromSignIn=true&trk=cold_join_sign_in');
@@ -24,9 +22,9 @@ async function linkedinPost(text) {
   await page.click('.btn__primary--large');
   
 
-  await page.waitForSelector('.artdeco-button');
+  await page.waitForSelector('button.artdeco-button.artdeco-button--muted.artdeco-button--4.artdeco-button--tertiary.ember-view.share-box-feed-entry__trigger');
 
-  await page.click('.artdeco-button');
+  await page.click('button.artdeco-button.artdeco-button--muted.artdeco-button--4.artdeco-button--tertiary.ember-view.share-box-feed-entry__trigger');
 
 
   await page.waitForSelector('div.ql-editor.ql-blank');
@@ -37,7 +35,7 @@ async function linkedinPost(text) {
   await setTimeout(2000);
 
   await page.click('.share-actions__primary-action.artdeco-button');
-
+console.log('posted');
   await browser.close();
 };
 
